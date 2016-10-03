d0af7f3ec0b18cea1eb1ef279a097f64c034df1e
nadjib <nadjib.chiheb@ruecentrale.com>
2016-10-03 23:30:46 +0200

    home.twig

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/home")
+     */
+    public function homeAction()
+    {
+        return $this->render('AppBundle:Home:home.html.twig', array(
+            // ...
+        ));
+    }
+
+}
